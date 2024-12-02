b91505cf85028647b731760e41d1b6a8fd44b494
Sophia Dai <sophiadai8@gmail.com>
2024-12-02 15:36:49 -0800

    clean up background.js

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -51,24 +51,6 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     case "wordUnderCursor":
       console.log("wordUnderCursor:", message.word);
       break;
-    // case "summarize":
-    //   // Return true to indicate we'll send a response asynchronously
-    //   (async () => {
-    //     try {
-    //       const result = await askLLM(
-    //         message.systemPrompt,
-    //         message.userPrompt,
-    //         message.apiKey
-    //       );
-    //       console.log("chrome.runtime.onMessage sendResponse", result);
-    //       sendResponse({ success: true, data: result });
-    //     } catch (error) {
-    //       console.error("Error in summarize:", error);
-    //       sendResponse({ success: false, error: error.message });
-    //     }
-    //   })();
-    //   return true;
-    //   break;
     case "LOG":
       const logStyle =
         "background: #a0a0a0; color: #000; padding: 2px 5px; border-radius: 3px;";
@@ -93,171 +75,6 @@ function sendNativeMessage(message) {
     console.error("Native messaging port is not connected");
   }
 }
-
-// function analyzeDOMWithChatGPT(apiKey) {
-//   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-//     if (tabs[0].url.startsWith("chrome://")) {
-//       chrome.runtime.sendMessage({
-//         type: "chatGPTResponse",
-//         response:
-//           "Unable to access chrome:// URLs. Please try on a different page.",
-//       });
-//     } else {
-//       chrome.scripting.executeScript(
-//         {
-//           target: { tabId: tabs[0].id },
-//           function: queryDOM,
-//         },
-//         (results) => {
-//           if (chrome.runtime.lastError) {
-//             console.error(chrome.runtime.lastError);
-//             chrome.runtime.sendMessage({
-//               type: "chatGPTResponse",
-//               response:
-//                 "An error occurred while querying the DOM: " +
-//                 chrome.runtime.lastError.message,
-//             });
-//             return;
-//           }
-//           console.log("DOM:", results);
-//           const domStructure = results[0].result;
-//           chrome.runtime.sendMessage({
-//             type: "domStructure",
-//             domStructure: domStructure,
-//           });
-//           const systemPrompt =
-//             "This is a input and button element from a webpage, is it a login page? \
-//             If yes, return the element id for the username, password and submit/next button. \
-//             output must be json object, has and only has three keys: username, password, submit; \
-//             If it is not a login page, return a empty json object.";
-//           sendChatGPTRequest(systemPrompt, domStructure, apiKey);
-//         }
-//       );
-//     }
-//   });
-// }
-
-// async function sendChatGPTRequest(systemPrompt, userPrompt, apiKey) {
-//   try {
-//     console.log("sendChatGPTRequest", systemPrompt, userPrompt);
-//     const response = await fetch("https://api.openai.com/v1/chat/completions", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//         Authorization: `Bearer ${apiKey}`,
-//       },
-//       body: JSON.stringify({
-//         model: "gpt-3.5-turbo",
-//         messages: [
-//           { role: "system", content: systemPrompt },
-//           { role: "user", content: userPrompt },
-//         ],
-//         temperature: 0.7,
-//         max_tokens: 150,
-//       }),
-//     });
-
-//     if (!response.ok) {
-//       throw new Error(`HTTP error! status: ${response.status}`);
-//     }
-
-//     const data = await response.json();
-//     const chatGPTResponse = data.choices[0].message.content;
-
-//     //
-
-//     console.log("chatgpt resp: ", chatGPTResponse);
-
-//     let chatgptResult = chatGPTResponse;
-//     try {
-//       if (chatgptResult.startsWith("```json")) {
-//         chatgptResult = chatgptResult
-//           .substring(7, chatgptResult.length - 3)
-//           .trim();
-//       } else if (
-//         chatgptResult.startsWith("```") &&
-//         chatgptResult.endsWith("```")
-//       ) {
-//         chatgptResult = chatgptResult
-//           .substring(3, chatgptResult.length - 3)
-//           .trim();
-//       }
-//       console.log("sendChatGPTRequest fill form with", chatgptResult);
-//       const formData = JSON.parse(chatgptResult);
-//       console.log("formData", formData);
-//       if (formData.username && formData.submit) {
-//         fillAndSubmitForm(formData);
-//         console.log("Form fill and submit request sent");
-//       } else {
-//         console.log("Invalid form data or not a login page");
-//         // chatgptResultElement.textContent = "Invalid form data or not a login page";
-//       }
-//     } catch (error) {
-//       console.error("Error parsing ChatGPT result:", error);
-//       // chatgptResultElement.textContent = "Error parsing ChatGPT result: " + error.message;
-//     }
-//     //
-
-//     chrome.runtime.sendMessage({
-//       type: "chatGPTResponse",
-//       response: chatGPTResponse,
-//     });
-//   } catch (error) {
-//     console.error("Error sending request to ChatGPT:", error);
-//     chrome.runtime.sendMessage({
-//       type: "chatGPTResponse",
-//       response:
-//         "Error: Unable to get response from ChatGPT. Please check your API key and try again.",
-//     });
-//   }
-// }
-
-
-// Helper Functions
-
-// function queryDOM() {
-//   const inputs = Array.from(document.querySelectorAll("input")).filter(
-//     (input) => input.type !== "hidden"
-//   );
-//   const buttons = document.querySelectorAll("button");
-
-//   let resultHTML = "<h3>Input Elements (excluding hidden):</h3><ul>";
-//   inputs.forEach((input, index) => {
-//     resultHTML += `<li>Input ${index + 1}: type="${input.type}", id="${input.id
-//       }", name="${input.name}"</li>`;
-//   });
-//   resultHTML += "</ul><h3>Button Elements:</h3><ul>";
-//   buttons.forEach((button, index) => {
-//     resultHTML += `<li>Button ${index + 1}: id="${button.id}", text="${button.textContent
-//       }"</li>`;
-//   });
-//   resultHTML += "</ul>";
-
-//   return resultHTML;
-// }
-
-// async function listHIDDevices() {
-//   try {
-//     const devices = await navigator.hid.requestDevice({ filters: [] });
-
-//     if (devices.length === 0) {
-//       console.log("No HID devices selected.");
-//       return;
-//     }
-
-//     console.log("Selected HID devices:");
-//     devices.forEach((device, index) => {
-//       console.log(`Device ${index + 1}:`);
-//       console.log(`  Product name: ${device.productName}`);
-//       console.log(`  Vendor ID: ${device.vendorId}`);
-//       console.log(`  Product ID: ${device.productId}`);
-//     });
-//   } catch (error) {
-//     console.error("Error accessing HID devices:", error);
-//   }
-// }
-
-// Native Messaging Setup
 
 function connectNativeHost() {
   port = chrome.runtime.connectNative(hostName);
@@ -295,39 +112,11 @@ function handleHidCmd(cmd, parameter) {
       });
     });
   } else if (cmd == "summarize") {
-
-      // chrome.windows.getCurrent({ populate: true }, (window) => {
-      //   chrome.sidePanel.open({ windowId: window.id });
-      // });
-    // // send cmd to sidepanel
-    // chrome.runtime.sendMessage({
-    //   type: "hid_cmd",
-    //   target: "side-panel",
-    //   action: cmd,
-    // });
     summarizeHandler();
   } else if (cmd == "click") {
-    // send cmd to sidepanel
-    // chrome.runtime.sendMessage({
-    //   type: "hid_cmd",
-    //   target: 'side-panel',
-    //   action: cmd,
-    //   parameter: parameter
-    // });
-
-    // chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    //   chrome.tabs.sendMessage(tabs[0].id, {
-    //     type: "hid_cmd",
-    //     target: "content-script",
-    //     action: cmd,
-    //     parameter: parameter,
-    //   });
-    // });
-
     console.log('Received voice input:', parameter);
 
     const matchedLink = findBestMatch(parameter);
-
     if (matchedLink) {
       console.log('Navigating to:', matchedLink.url);
       // Navigate the current tab to the matched URL
